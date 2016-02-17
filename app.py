7ce95e76f41c67ba9ae86a1f50837b09d0bf25d3
Rachel Ann Luna <rachelannluna@cysuber-chey.com>
2016-02-17 12:07:25 +0800

    Added login logic

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
 
 @app.route('/')
-def login():
+def home():
     return render_template('login.html')
+
+@app.route('/login/', methods=['POST'])
+def login():
+    session['username'] = request.form['username']
+    session['password'] = request.form['password']
+
+    return render_template('home.html')
 
 app.secret_key = 'jff6sdrg76gduoiwenjr'
 
